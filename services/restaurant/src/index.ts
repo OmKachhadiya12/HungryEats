5dc3243d2cac417c5c99574d24c751fd32e1dd04
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-07-27 12:14:09 +0530

    Add the Address controllers and routes.

--- services/restaurant/src/index.ts
+++ services/restaurant/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import restaurantRoutes from "./routes/restaurant.js";
 import itemRoutes from "./routes/menuitem.js";
 import cartRoutes from "./routes/cart.js";
+import addressRoutes from "./routes/address.js";
 import cors from "cors";
 
 dotenv.config();
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/restaurant",restaurantRoutes);
 app.use("/api/item",itemRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/address",addressRoutes);
 
 app.listen(PORT,() => {
     console.log(`Restaurant service is running on ${PORT}.`);
