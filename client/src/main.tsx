31593ec586f79240638c2f67e7a5029d90c52f1b
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-08-01 12:20:55 +0530

    Add the socket.io-client in the frontend and socketContext.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,17 +4,21 @@ import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AppProvider } from './context/AppContext.tsx';
+import { SocketProvider } from './context/SocketContext.tsx';
 import "leaflet/dist/leaflet.css"
 
 export const authService = "http://localhost:1212";
 export const restaurantService = "http://localhost:1213";
 export const utilsService = "http://localhost:1214";
+export const realTimeService = "http://localhost:1215";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
       <AppProvider>
+        <SocketProvider>
           <App />
+        </SocketProvider>
       </AppProvider>
     </GoogleOAuthProvider>
   </StrictMode>
