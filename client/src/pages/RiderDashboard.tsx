af3b1f887a548ab8d6b50406f334420f94434b80
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-08-06 17:07:08 +0530

    Add the leaflet-routing-machine for the Rider dashboard.

--- client/src/pages/RiderDashboard.tsx
+++ client/src/pages/RiderDashboard.tsx
@@ -9,6 +9,7 @@ import type { IOrder } from "../types";
 import audio from "../assets/faaah.mp3";
 import RiderOrderRequest from "../components/RiderOrderRequest";
 import RiderCurrentOrder from "../components/RiderCurrentOrder";
+import RiderOrderMap from "../components/RiderOrderMap";
 
 interface IRider {
   _id: string;
